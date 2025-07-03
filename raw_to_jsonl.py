1f0a123d3cbabd4e452889edfb9bc0ad45dffb4e
Dowiw <monjie12272@gmail.com>
2025-07-03 13:48:56 +0200

    add: jsonl format, and current data

---
+++ raw_to_jsonl.py
@@ -0,0 +1,9 @@
+import json
+
+# convert the json file to a json one line file
+with open('student_job_hunt_berlin_cleaned.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+with open('student_job_hunt_berlin_cleaned.jsonl', 'w', encoding='utf-8') as f:
+    for item in data:
+        f.write(json.dumps(item) + "\n")
