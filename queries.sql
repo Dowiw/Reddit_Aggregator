103bc71e49dbf20a8924b1e356a0078dc0935808
Dowiw <monjie12272@gmail.com>
2025-07-04 14:06:20 +0200

    add: giving out raw file for sharing

--- queries.sql
+++ queries.sql
@@ -16,3 +16,16 @@ CREATE TABLE reddit_posts (
 -- delete table if needed
 DROP TABLE reddit_posts;
 
+-- show the path of the config for collaboration
+SHOW config_file;
+
+-- show hba file for ip permissions
+SHOW hba_file;
+
+-- make readonly user
+CREATE USER readonly_user WITH PASSWORD 'readonlypass';
+GRANT CONNECT ON DATABASE "reddit-data" TO readonly_user;
+GRANT USAGE ON SCHEMA public TO readonly_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+
+SELECT * FROM reddit_posts;
